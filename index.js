084e8e4349656dc839cc85c6d5dd1c146ec03d03
Marcelo Oikawa <marcelo.oikawa@olxbr.com>
2018-10-08 02:01:51 -0300

    adding callback

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ module.exports = async (url, opts) => {
         try {
             return await fetch(url, opts)
         } catch(e) {
+            if (opts.callback) {
+                opts.callback(retry)
+            }
             retry = retry - 1
             if (retry == 0) {
                 throw e
