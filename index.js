3fcff0ff6ecc0a03b6697336a971c028201dde95
David Gonzalez <davidjgonzalez@gmail.com>
2020-08-27 09:26:01 -0400

    #3 - retry pause

--- index.js
+++ index.js
@@ -2,17 +2,30 @@ const fetch = require('node-fetch');
 
 module.exports = async (url, opts) => {
     let retry = opts && opts.retry || 3
+    let pause = (opts && opts.pause && opts.pause > 0) || 0
+
     while (retry > 0) {
         try {
             return await fetch(url, opts)
         } catch(e) {
             if (opts.callback) {
                 opts.callback(retry)
-            }
+            }        
             retry = retry - 1
             if (retry == 0) {
                 throw e
             }
+
+            if (opts.pause) {
+                console.log("pausing..");
+                await sleep(opts.pause);
+                console.log("done pausing...");
+            }
         }
     }
 };
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
--- test/fetch-retry.test.js
+++ test/fetch-retry.test.js
@@ -51,8 +51,6 @@ describe('node-fetch-retry test ', async function () {
     });
 
     it('should raise an error because could not find URL', async function () {
-        this.timeout(10000);
-
         let calls = []
         let opts = {
             retry: 5,
@@ -65,6 +63,25 @@ describe('node-fetch-retry test ', async function () {
             should.fail('no error was thrown when for fake URL')
         } catch (e) {
             calls.should.have.lengthOf(5)
+        }
+    });
+
+    it('should work properly with pause', async function () {
+        this.timeout(10000);
+
+        let calls = []
+        let opts = {
+            retry: 3,
+            pause: 1000, // in milliseconds
+            callback: retry => {
+                calls.push(new Date().getTime())
+            }
+        }
+        try {
+            await fetch('https://fakeURLfake', opts)
+        } catch (e) {
+            should.equal(true, calls[1] - calls[0] >= 1000);
+            should.equal(true, calls[2] - calls[1] >= 1000);
         }
     });
 })
