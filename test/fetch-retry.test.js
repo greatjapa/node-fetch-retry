5b3d20c5fe5ea83e66f7994afb3cc136406095be
David Bell <dtbell99@gmail.com>
2020-11-30 16:59:55 -0500

    Added silent functionality

--- test/fetch-retry.test.js
+++ test/fetch-retry.test.js
@@ -66,7 +66,32 @@ describe('node-fetch-retry test ', async function () {
         }
     });
 
-    it('should work properly with pause', async function () {
+    it('should work properly with pause and silent', async function () {
+        this.timeout(10000);
+
+        let calls = []
+        let opts = {
+            retry: 3,
+            pause: 1000, // in milliseconds
+            callback: retry => {
+                calls.push(new Date().getTime())
+            },
+            silent: true
+        }
+        let cntr = 0;
+        console.log = () => {
+            cntr++;
+        }
+        try {
+            await fetch('https://fakeURLfake', opts)
+        } catch (e) {
+            should.equal(true, calls[1] - calls[0] >= 1000);
+            should.equal(true, calls[2] - calls[1] >= 1000);
+        }
+        should.equal(cntr, 0);
+    });
+
+    it('should work properly with pause and not silent', async function () {
         this.timeout(10000);
 
         let calls = []
@@ -77,11 +102,16 @@ describe('node-fetch-retry test ', async function () {
                 calls.push(new Date().getTime())
             }
         }
+        let cntr = 0;
+        console.log = (message) => {
+            cntr++;
+        }
         try {
             await fetch('https://fakeURLfake', opts)
         } catch (e) {
             should.equal(true, calls[1] - calls[0] >= 1000);
             should.equal(true, calls[2] - calls[1] >= 1000);
         }
+        should.equal(cntr, 4);
     });
 })
